56ca471741739273a6c5b81c179600cc3a9d883b
Sam Protsenko <joe.skb7@gmail.com>
2013-10-21 03:07:50 +0300

    car: add "vehicle_move()" function
    Signed-off-by: Sam Protsenko <joe.skb7@gmail.com>

--- car/include/motion/vehicle.h
+++ car/include/motion/vehicle.h
@@ -1,7 +1,14 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
+#define X_LEFT		0x01
+#define X_RIGHT		0x02
+#define Y_UP		0x10
+#define Y_DOWN		0x20
+
 void vehicle_init(void);
+
+void vehicle_move(int flags);
 
 void vehicle_forward(void);
 void vehicle_forward_left(void);
--- car/src/motion/vehicle.c
+++ car/src/motion/vehicle.c
@@ -52,3 +52,38 @@ void vehicle_backward_right(void)
 {
 	md_command(M1_REVERSAL | M2_STOP);
 }
+
+void vehicle_move(int flags)
+{
+	switch (flags) {
+	case 0:
+		vehicle_stop();
+		break;
+	case Y_UP:
+		vehicle_forward();
+		break;
+	case Y_DOWN:
+		vehicle_backward();
+		break;
+	case X_LEFT:
+		vehicle_turn_left();
+		break;
+	case X_RIGHT:
+		vehicle_turn_right();
+		break;
+	case X_LEFT + Y_UP:
+		vehicle_forward_left();
+		break;
+	case X_LEFT + Y_DOWN:
+		vehicle_backward_left();
+		break;
+	case X_RIGHT + Y_UP:
+		vehicle_forward_right();
+		break;
+	case X_RIGHT + Y_DOWN:
+		vehicle_backward_right();
+		break;
+	default:
+		return;
+	}
+}
